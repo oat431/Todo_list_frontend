bdf4a9d59edff23fd5efd6cf63ec3fd3ea8fdbe0
Oralita <oat431@gmail.com>
2021-06-14 14:05:05 +0700

    [ 🪁 init ] : the frontend project

---
+++ src/View/TodoList.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import Axios from "axios";
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo_list: [],
+    };
+  }
+
+  componentDidMount() {
+    Axios.get("http://167.99.64.31:5039/api/todo/todolist", {
+      header: { "Access-Control-Allow-Origin": "*" },
+    })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ todo_list: res.data });
+      })
+      .catch((err) => {
+        console.log("some err happen");
+      });
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xl={12} md={12}>
+            <h1>This is the all todo list</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xl={12} md={12}>
+            <ListGroup as="ul">
+              {this.state.todo_list.map((item, index) => {
+                return (
+                  <ListGroup.Item as="li">
+                    <strong>{(index + 1) + " " + item.title}</strong> update on{" "}{item.date}
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default TodoList;
